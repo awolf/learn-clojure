4508cd8e773f83cceb8c55fe4606ac31c94a32bd
Adam Wolf <adam@adamjwolf.com>
2018-12-26 15:58:27 -0700

    Finished chapter 1

---
+++ prg-clojure/introduction.clj
@@ -0,0 +1,9 @@
+
+(def visitors (atom #{}))
+
+(defn hello
+  "Writes hello message to *out*. Calls you by username.
+    Knows if you have been here before."
+  [username]
+  (swap! visitors conj username)
+  (str "hello, " username))
